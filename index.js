2366c10db46a04824b143a1d592b57cc89152dd6
makerboomRijnland <makerboom@mborijnland.nl>
2023-06-15 12:15:28 +0200

    Update filters and avatar

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ function showStudent(student) {
     const studentTemplate = document.getElementById('student-template');
     const studentArticle = studentTemplate.content.cloneNode(true);
 
-    studentArticle.querySelector('.name').innerHTML = student.name;
+    const name = `${student.firstName} ${student.lastName}`;
+    studentArticle.querySelector('.name').innerHTML = name;
     studentArticle.querySelector('.age').innerHTML = student.age;
     studentArticle.querySelector('.group').innerHTML = student.group;
+
+    const avatarURL = `https://i.pravatar.cc/150?u=${student.lastName}`;
+    studentArticle.querySelector('.avatar').src = avatarURL;
 
     studentsSection.appendChild(studentArticle);
 }
@@ -17,33 +21,41 @@ function loadStudents() {
         .then((response) => response.json())
         .then((data) => showStudents(data));
 
-    const ageFilterSelect = document.getElementById('students-filter-age');
-    const groupFilterSelect = document.getElementById('students-filter-group');
+        const groupFilterSelect = document.getElementById('students-filter-group');
+        groupFilterSelect.addEventListener('change', filterStudents);
 
-    ageFilterSelect.addEventListener('change', filterStudents);
-    groupFilterSelect.addEventListener('change', filterStudents);
+    // const ageFilterSelect = document.getElementById('students-filter-age');
+    // const groupFilterSelect = document.getElementById('students-filter-group');
+
+    // ageFilterSelect.addEventListener('change', filterStudents);
+    // groupFilterSelect.addEventListener('change', filterStudents);
 }
 
 function filterStudents() {
-    const studentsSection  = document.getElementById('students');
-    const studentArticles = studentsSection.querySelectorAll('.student');
-
-    const ageFilterSelect = document.getElementById('students-filter-age');
-    const ageFilter = ageFilterSelect.value;
-
     const groupFilterSelect = document.getElementById('students-filter-group');
     const groupFilter = groupFilterSelect.value;
 
-    for(let studentArticle of studentArticles) {
-        const studentAge = studentArticle.querySelector('.age').innerHTML;
-        const studentGroup = studentArticle.querySelector('.group').innerHTML;
-        
-        const isCorrectAge = ageFilter == "--" || studentAge == ageFilter;
-        const isCorrectGroup = groupFilter == "--" || studentGroup == groupFilter;
+    // alert("Filter: " + groupFilter);
 
-        if(isCorrectAge && isCorrectGroup) {
+    const studentsSection  = document.getElementById('students');
+    const studentArticles = studentsSection.querySelectorAll('.student');
+
+    // const ageFilterSelect = document.getElementById('students-filter-age');
+    // const ageFilter = ageFilterSelect.value;
+
+    // const groupFilterSelect = document.getElementById('students-filter-group');
+    // const groupFilter = groupFilterSelect.value;
+
+    for(let studentArticle of studentArticles) {
+        const studentGroup = studentArticle.querySelector('.group').innerHTML;
+        const groupFilterEmpty = groupFilter == "--";
+        const isCorrectGroup = groupFilterEmpty || groupFilter == studentGroup;
+
+        if(isCorrectGroup) {
+            // Toon de student
             studentArticle.style.display = 'block';
         } else {
+            // Verberg de student
             studentArticle.style.display = 'none';
         }
     }
